236c53827ac356bb3e557a59d8ae007b9c962655
chaitu <chaitus.ch09@gmail.com>
2024-09-19 14:21:42 -0700

    changed the logic

--- mysql.sh
+++ mysql.sh
@@ -11,12 +11,12 @@ dnf install mysql-server -y
 systemctl enable mysqld
 systemctl start mysqld
 
-# setup mysql server password
-#echo 'show databases'|mysql -h servername -uroot -pExpenseApp@1
-#if [ $? -ne 0 ]; then
- # mysql_secure_installation --set-root-pass ${mysql_root_password}
-  #fi
-
+ setup mysql server password
+echo 'show databases'|mysql -h servername -uroot -pExpenseApp@1
+if [ $? -ne 0 ]; then
 mysql_secure_installation --set-root-pass ${mysql_root_password}
+fi
+
+#mysql_secure_installation --set-root-pass ${mysql_root_password}
 #ExpenseApp@1
 #mysql -u root -p${mysql_root_password}
